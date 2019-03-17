1d71cb700575de71795459b5eaa15f7e92092621
mmirca <marius.mirca@gmail.com>
2019-03-17 17:07:08 +0100

    diseño city-destroyer implementado

--- run-game.component.js
+++ run-game.component.js
@@ -22,7 +22,7 @@ class RunGame extends React.Component {
       }, 1000);
   }
   componentWillUnmount() {
-      clearInterval(this.interval);
+    clearInterval(this.interval);
   }
   incrementClickCount() {
     if (this.state.clickCount >= this.winClickCount) {
@@ -31,15 +31,27 @@ class RunGame extends React.Component {
     }
     this.setState({clickCount: this.state.clickCount + 1});
   }
+  getDestroyedLevel(buildingWeakness) {
+    const destroyedPercentage = Math.round(this.state.clickCount / (this.winClickCount / buildingWeakness) * 100);
+    if (destroyedPercentage >= 100) {
+      return 90;
+    }
+    return destroyedPercentage;
+  }
   render() {
     return(<div>
-      <h2 class="h4 mb-4 pb-2 border-bottom">{ (this.props.username || 'user').toUpperCase() }</h2>
-      <p>¡Ánimo ya llevas <b>{ this.state.clickCount }</b> clicks!</p>
-      <p class="mb-4">Te faltan solo <b>{ this.winClickCount - this.state.clickCount }</b> clicks para ganar</p>
-      <button ref={ this.$button } onClick={ this.incrementClickCount.bind(this) } class="btn btn-block btn-primary mb-5">CLICK</button>
-      <div class="progress">
+      <h2 class="h4 mb-4 pb-2 border-bottom">{ (this.props.username || 'user').toUpperCase() } EL DESTRUCTOR</h2>
+      <div class="city">
+        <img style={{transform: `translateY(${ this.getDestroyedLevel(10) }%)`}} class="city__building city__building--05" src="assets/img/building_05.png"/>
+        <img style={{transform: `translateY(${ this.getDestroyedLevel(8) }%)`}} class="city__building city__building--04" src="assets/img/building_04.png"/>
+        <img style={{transform: `translateY(${ this.getDestroyedLevel(4) }%) translateX(-50%)`}} class="city__building city__building--03" src="assets/img/building_03.png"/>
+        <img style={{transform: `translateY(${ this.getDestroyedLevel(2) }%) translateX(-50%)`}} class="city__building city__building--02" src="assets/img/building_02.png"/>
+        <img style={{transform: `translateY(${ this.getDestroyedLevel(1) }%) translateX(-50%)`}} class="city__building city__building--01" src="assets/img/building_01.png"/>
+      </div>
+      <div class="progress mb-4">
         <div className={`progress-bar bg-${ this.state.progressClass }`} style={{width: `${ this.state.progress }%`, transition: `all 1s linear`}}></div>
       </div>
+      <button ref={ this.$button } onClick={ this.incrementClickCount.bind(this) } class="btn btn-block btn-primary mb-5">DESTRUYE</button>
     </div>);
   }
 }
